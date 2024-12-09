47c185eae88959565eae5f1c32411f0e2322716b
Nicholas Hubbard <nicholashubbard@posteo.net>
2024-12-09 12:34:59 -0500

    add test_backendbase.py

---
+++ tests/test_yaesm/test_backend/test_backendbase.py
@@ -0,0 +1,1 @@
+# Until (unless) we can come up with a clean and simple way to generically test the backends, every backend test script should have their own tests for the abstract methods in the BackendBase class.
