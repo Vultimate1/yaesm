04b760b0600d58b5b3b96da816d75a145655bf84
SkiHatDuckie <SkiHatDuckie@gmail.com>
2025-04-16 13:51:15 -0400

    Construct backup in config parser

--- src/yaesm/config.py
+++ src/yaesm/config.py
@@ -1,5 +1,7 @@
 import yaml
 
+from yaesm.backup import *
+from yaesm.sshtarget import *
 from yaesm.timeframe import *
 
 class SafeLineLoader(yaml.loader.SafeLoader):
@@ -35,7 +37,7 @@ def construct_timeframes(backup_spec, timeframe_type) -> dict:
     if append_missing_keys(result["missing_specs"], backup_spec, settings) == 0:
         for setting in settings:
             setting_type = setting[setting.rfind("_") + 1:]
-            if (setting_type == "days"):
+            if setting_type == "days":
                 check_validity(validity_checks[setting], backup_spec[setting])
             else:
                 check_validity(validity_checks[setting_type], backup_spec[setting])
@@ -64,26 +66,61 @@ def handle_timeframes(backup_spec) -> list:
 
     return timeframes, missing_specs, bad_specs
 
-def handle_backups(backup_spec, timeframes) -> list:
-    """Returns 3 lists containing the successfully instantiated backups, missing
-    specifications and bad specifications, in that order."""
-    backups = []
-    bad_specs = []
-    missing_specs = []
-    match backup_spec["backend"]:
-        case "rsync":
-            pass
-        case "btrfs":
-            pass
-        case "zfs":
-            pass
-        case _:
-            bad_specs["backend"] = [backup_spec["__line__"], backup_spec["backend"]]
+def construct_backup(backup_name, backup_spec, timeframes):
+    """Returns a result code and the type of data corresponding to said code.
+    Whether specified directories exist or not is not checked.
+    
+    0 = `Backup` (success)
+    
+    1 = missing specifications (failure)
+    
+    2 = bad specifications (failure)"""
+    if backup_spec["backend"] == "zfs":
+        target_setting_names = ["src_dataset", "dst_dataset"]
+    else:
+        target_setting_names = ["src_dir", "dst_dir"]
 
-    return backups, missing_specs, bad_specs
+    # Handle required specs
+    result = []
+    src = None, dst = None;
+    if append_missing_keys(result, backup_spec, target_setting_names) == 0:
+        src = backup_spec[target_setting_names[0]]
+        dst = backup_spec[target_setting_names[1]]
+        src_is_ssh = is_sshtarget(src), dst_is_ssh = is_sshtarget(dst)
+        if src_is_ssh and dst_is_ssh:
+            result.append([backup_spec[target_setting_names[0]]["__line__"], src_is_ssh])
+        elif src_is_ssh or dst_is_ssh:
+            if append_missing_keys(result, backup_spec, ["ssh_key"]) == 0:
+                ssh_key = backup_spec["ssh_key"]
+                ssh_config = backup_spec.get("ssh_config")
+                if src_is_ssh:
+                    src = SSHTarget(src, ssh_key, sshconfig=ssh_config)
+                if dst_is_ssh:
+                    dst = SSHTarget(dst, ssh_key, sshconfig=ssh_config)
+            else:
+                return 1, result
+    else:
+        return 1, result
+
+    # Construct backup & handle optional specs
+    if backup_spec["backend"] == "rsync":
+        if exclude := backup_spec.get("exclude", False) and exclude is not list:
+            result.append([backup_spec["exclude"]["__line__"], type(exclude)])
+        if exclude_from := backup_spec.get("exclude_from", False) and exclude_from is not str:
+            result.append([backup_spec["exclude"]["__line__"], type(exclude)])
+        if len(result) == 0:
+            return 0, Backup(backup_name, src, dst, timeframes, exclude=exclude,
+                          exclude_from=exclude_from)
+    elif backup_spec["backend"] == "btrfs" or backup_spec["backend"] == "zfs":
+        if len(result) == 0:
+            return 0, Backup(backup_name, src, dst, timeframes)
+    else:
+        result = [backup_spec["__line__"], backup_spec["backend"]]
+
+    return 2, result
 
 
-def parse_file(config_path):
+def parse_file(config_path) -> list:
     """Returns a list of backups, given a valid YAML config file.
 
     Throws `OSError` if unable to open `config_path`,
@@ -104,8 +141,14 @@ def parse_file(config_path):
 
             timeframes, temp, bad_specs["timeframes"] = handle_timeframes(backup_spec)
             missing_specs.extend(temp)
-            backups, temp, bad_specs["backups"] = handle_backups(backup_spec, timeframes)
-            missing_specs.extend(temp)
+            code, result = construct_backup(backup_name, backup_spec, timeframes)
+            match code:
+                case 0:
+                    backups.append(result)
+                case 1:
+                    missing_specs.extend(result)
+                case 2:
+                    bad_specs["backend"] = result
 
             if len(missing_specs) != 0 or len(bad_specs.keys()) != 0:
                 invalid_input_info[backup_name] = [missing_specs, bad_specs]
