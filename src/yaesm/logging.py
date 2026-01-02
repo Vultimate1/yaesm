4d0fdc9457e778aa2d3ff7df3e55aa68f9360cb3
SkiHatDuckie <SkiHatDuckie@gmail.com>
2026-01-01 21:07:13 -0500

    Cleanup logging + tests

--- src/yaesm/logging.py
+++ src/yaesm/logging.py
@@ -1,12 +1,55 @@
-# This module is a wrapper over pythons logging module. All logging in yaesm
-# should happen through the functions defined in this module.
+"""src/yaesm/logging.py
+
+This module is a wrapper over pythons logging module. All logging in yaesm
+should happen through the functions defined in this module."""
 
 import logging
 import logging.handlers
 import inspect
 
-class LoggingNotInitializedException(Exception):
-    ...
+class Logging:
+    def __init__(self, stderr=True, logfile=None, syslog=False, syslog_address="/dev/log",
+                 level=logging.INFO):
+        """Initialize logging for yaesm. Yaesm can log to any and all of stderr,
+        syslog, and a file. If this function is called multiple times then it will
+        fully re-initialize the logging. If none of `stderr`, `logfile`, or `syslog`
+        are True, then `stderr` is set to True.
+
+        Note that yaesm supports only two levels of logging, INFO and DEBUG."""
+        if not (stderr or logfile or syslog):
+            stderr = True
+        formatter = logging.Formatter("yaesm - %(asctime)s - %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S")
+        handlers = []
+        if syslog:
+            syslog_handler = logging.handlers.SysLogHandler(address=syslog_address)
+            syslog_handler.setFormatter(formatter)
+            syslog_handler.setLevel(level)
+            handlers.append(syslog_handler)
+        if stderr:
+            stderr_handler = logging.StreamHandler() # defaults to sys.stderr
+            stderr_handler.setFormatter(formatter)
+            stderr_handler.setLevel(level)
+            handlers.append(stderr_handler)
+        if logfile:
+            logfile_handler = logging.FileHandler(logfile, encoding="utf-8")
+            logfile_handler.setFormatter(formatter)
+            logfile_handler.setLevel(level)
+            handlers.append(logfile_handler)
+        root_logger = logging.getLogger()
+        root_logger.setLevel(level)
+        for handler in root_logger.handlers[:]:
+            root_logger.removeHandler(handler)
+        for handler in handlers:
+            root_logger.addHandler(handler)
+    
+    def get(self, name=None):
+        """Return a logger with the specified name. If name is None then it defaults
+        to the name of the callers module. If logging has not yet been initialized
+        (i.e. init_logging() has not been invoked), then raise an exception.
+        """
+        if name is None:
+            name = inspect.getmodule(inspect.stack()[1][0]).__name__
+        return logging.getLogger(name)
 
 _logging_initialized = False
 
--- tests/test_yaesm/test_logging.py
+++ tests/test_yaesm/test_logging.py
@@ -1,52 +1,49 @@
-import pytest
+"""tests/test_yaesm/test_logging.py"""
+
 import logging
-import subprocess
 import re
 
-from yaesm.logging import logger, init_logging, disable_logging, LoggingNotInitializedException
-
-def test_raises_logging_not_initialized():
-    with pytest.raises(LoggingNotInitializedException):
-        logger()
+from yaesm.logging import Logging
 
 def test_init_logging():
-    init_logging(stderr=True)
-    assert len(logger("").handlers) == 1
-    assert logger("").level == logging.INFO
+    logger = Logging(stderr=True)
+    assert len(logger.get("").handlers) == 1
+    assert logger.get("").level == logging.INFO
 
-    init_logging(syslog=True, stderr=True, logfile="/var/log/yaesm_test_logging.log", level=logging.DEBUG)
-    assert len(logger("").handlers) == 3
-    assert logger("").level == logging.DEBUG
+    logger = Logging(syslog=True, stderr=True, logfile="/var/log/yaesm_test_logging.log",
+                     level=logging.DEBUG)
+    assert len(logger.get("").handlers) == 3
+    assert logger.get("").level == logging.DEBUG
 
-    init_logging()
-    assert len(logger("").handlers) == 1
-    assert logger("").level == logging.INFO
+    logger = Logging()
+    assert len(logger.get("").handlers) == 1
+    assert logger.get("").level == logging.INFO
 
 def test_stderr_logging(capsys):
-    init_logging(stderr=True, level=logging.DEBUG)
-    logger().debug("TEST LOG")
+    logger = Logging(stderr=True, level=logging.DEBUG)
+    logger.get().debug("TEST LOG")
     assert re.match("^yaesm |.+DEBUG.+TEST LOG$", capsys.readouterr().err)
 
 def test_level_respected(capsys):
-    init_logging(stderr=True) # level defaults to INFO
-    logger().debug("TEST LOG")
+    logger = Logging(stderr=True) # level defaults to INFO
+    logger.get().debug("TEST LOG")
     assert "" == capsys.readouterr().err
 
-    logger().error("TEST LOG")
+    logger.get().error("TEST LOG")
     assert re.match(".+ERROR.+TEST LOG$", capsys.readouterr().err)
 
 def test_logfile_logging(path_generator):
     logfile = path_generator("yaesm_test_logging")
-    init_logging(logfile=logfile)
-    logger().info("TEST LOG")
+    logger = Logging(logfile=logfile)
+    logger.get().info("TEST LOG")
     assert logfile.is_file()
     assert re.match(".+INFO.+TEST LOG$", logfile.read_text())
 
 def test_syslog_logging():
-    init_logging(syslog=True)
-    logger().info("TEST LOG SYSLOG")
+    logger = Logging(syslog=True)
+    logger.get().info("TEST LOG SYSLOG")
     found_log = False
-    with open("/var/log/syslog", "r") as syslog:
+    with open("/var/log/syslog", "r", encoding="utf-8") as syslog:
         for line in syslog:
             if re.match(".+INFO.+TEST LOG SYSLOG", line):
                 found_log = True
@@ -55,8 +52,8 @@ def test_syslog_logging():
 
 def test_multi_dest_logging(capsys, path_generator):
     logfile = path_generator("yaesm_test_logging")
-    init_logging(stderr=True, logfile=logfile)
-    logger().info("TEST LOG MULTI DEST")
+    logger = Logging(stderr=True, logfile=logfile)
+    logger.get().info("TEST LOG MULTI DEST")
     assert re.match(".+INFO.+TEST LOG MULTI DEST$", capsys.readouterr().err)
     assert re.match(".+INFO.+TEST LOG MULTI DEST$", logfile.read_text())
 
