09ef3e086e5ec44343bc04ba081410e9ea4f2d52
SkiHatDuckie <SkiHatDuckie@gmail.com>
2026-01-04 20:55:01 -0500

    Make `Logging` static

--- src/yaesm/logging.py
+++ src/yaesm/logging.py
@@ -7,8 +7,14 @@ import logging
 import logging.handlers
 import inspect
 
+class LoggingNotInitializedException(Exception):
+    ...
+
 class Logging:
-    def __init__(self, stderr=True, logfile=None, syslog=False, syslog_address="/dev/log",
+    _logging_initialized = False
+
+    @staticmethod
+    def initialize(stderr=False, logfile=None, syslog=False, syslog_address="/dev/log",
                  level=logging.INFO):
         """Initialize logging for yaesm. Yaesm can log to any and all of stderr,
         syslog, and a file. If this function is called multiple times then it will
@@ -41,73 +47,26 @@ class Logging:
             root_logger.removeHandler(handler)
         for handler in handlers:
             root_logger.addHandler(handler)
-    
-    def get(self, name=None):
+        Logging._logging_initialized = True
+
+    @staticmethod
+    def get(name=None):
         """Return a logger with the specified name. If name is None then it defaults
         to the name of the callers module. If logging has not yet been initialized
-        (i.e. init_logging() has not been invoked), then raise an exception.
-        """
+        (i.e. init_logging() has not been invoked), then raise an exception."""
+        if not Logging._logging_initialized:
+            raise LoggingNotInitializedException(
+                "trying to get logger but logging has not been initialized")
         if name is None:
             name = inspect.getmodule(inspect.stack()[1][0]).__name__
         return logging.getLogger(name)
 
-_logging_initialized = False
-
-def init_logging(stderr=True, logfile=None, syslog=False, syslog_address="/dev/log", level=logging.INFO):
-    """Initialize logging for yaesm. Yaesm can log to any and all of stderr,
-    syslog, and a file. If this function is called multiple times then it will
-    fully re-initialize the logging. If none of 'stderr', 'logfile', or 'syslog'
-    are True, then 'stderr' is set to True.
-
-    Note that yaesm supports only two levels of logging, INFO and DEBUG.
-    """
-    if not (stderr or logfile or syslog):
-        stderr = True
-    formatter = logging.Formatter("yaesm - %(asctime)s - %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S")
-    handlers = []
-    if syslog:
-        syslog_handler = logging.handlers.SysLogHandler(address=syslog_address)
-        syslog_handler.setFormatter(formatter)
-        syslog_handler.setLevel(level)
-        handlers.append(syslog_handler)
-    if stderr:
-        stderr_handler = logging.StreamHandler() # defaults to sys.stderr
-        stderr_handler.setFormatter(formatter)
-        stderr_handler.setLevel(level)
-        handlers.append(stderr_handler)
-    if logfile:
-        logfile_handler = logging.FileHandler(logfile, encoding="utf-8")
-        logfile_handler.setFormatter(formatter)
-        logfile_handler.setLevel(level)
-        handlers.append(logfile_handler)
-    root_logger = logging.getLogger()
-    root_logger.setLevel(level)
-    for handler in root_logger.handlers[:]:
-        root_logger.removeHandler(handler)
-    for handler in handlers:
-        root_logger.addHandler(handler)
-    global _logging_initialized
-    _logging_initialized = True
-
-def disable_logging():
-    """Disable all logging by removing all handlers and marking logging as uninitialized."""
-    root_logger = logging.getLogger()
-    for handler in root_logger.handlers[:]:
-        root_logger.removeHandler(handler)
-        handler.close()
-    root_logger.setLevel(logging.CRITICAL + 1)
-    global _logging_initialized
-    _logging_initialized = False
-
-def logger(name=None):
-    """Return a logger with the specified name. If name is None then it defaults
-    to the name of the callers module. If logging has not yet been initialized
-    (i.e. init_logging() has not been invoked), then raise an exception.
-    """
-    global _logging_initialized
-    if not _logging_initialized:
-        raise LoggingNotInitializedException("trying to get logger but logging has not been initialized")
-    if name is None:
-        name = inspect.getmodule(inspect.stack()[1][0]).__name__
-    logger = logging.getLogger(name)
-    return logger
+    @staticmethod
+    def disable():
+        """Disable all logging by removing all handlers and marking logging as uninitialized."""
+        root_logger = logging.getLogger()
+        for handler in root_logger.handlers[:]:
+            root_logger.removeHandler(handler)
+            handler.close()
+        root_logger.setLevel(logging.CRITICAL + 1)
+        Logging._logging_initialized = False
--- tests/test_yaesm/test_logging.py
+++ tests/test_yaesm/test_logging.py
@@ -3,45 +3,51 @@
 import logging
 import re
 
-from yaesm.logging import Logging
+import pytest
+
+from yaesm.logging import Logging, LoggingNotInitializedException
+
+def test_raises_logging_not_initialized():
+    with pytest.raises(LoggingNotInitializedException):
+        Logging.get()
 
 def test_init_logging():
-    logger = Logging(stderr=True)
-    assert len(logger.get("").handlers) == 1
-    assert logger.get("").level == logging.INFO
+    Logging.initialize(stderr=True)
+    assert len(Logging.get("").handlers) == 1
+    assert Logging.get("").level == logging.INFO
 
-    logger = Logging(syslog=True, stderr=True, logfile="/var/log/yaesm_test_logging.log",
+    Logging.initialize(syslog=True, stderr=True, logfile="/var/log/yaesm_test_logging.log",
                      level=logging.DEBUG)
-    assert len(logger.get("").handlers) == 3
-    assert logger.get("").level == logging.DEBUG
+    assert len(Logging.get("").handlers) == 3
+    assert Logging.get("").level == logging.DEBUG
 
-    logger = Logging()
-    assert len(logger.get("").handlers) == 1
-    assert logger.get("").level == logging.INFO
+    Logging.initialize()
+    assert len(Logging.get("").handlers) == 1
+    assert Logging.get("").level == logging.INFO
 
 def test_stderr_logging(capsys):
-    logger = Logging(stderr=True, level=logging.DEBUG)
-    logger.get().debug("TEST LOG")
+    Logging.initialize(stderr=True, level=logging.DEBUG)
+    Logging.get().debug("TEST LOG")
     assert re.match("^yaesm -.+DEBUG.+TEST LOG$", capsys.readouterr().err)
 
 def test_level_respected(capsys):
-    logger = Logging(stderr=True) # level defaults to INFO
-    logger.get().debug("TEST LOG")
+    Logging.initialize(stderr=True) # level defaults to INFO
+    Logging.get().debug("TEST LOG")
     assert "" == capsys.readouterr().err
 
-    logger.get().error("TEST LOG")
+    Logging.get().error("TEST LOG")
     assert re.match(".+ERROR.+TEST LOG$", capsys.readouterr().err)
 
 def test_logfile_logging(path_generator):
     logfile = path_generator("yaesm_test_logging")
-    logger = Logging(logfile=logfile)
-    logger.get().info("TEST LOG")
+    Logging.initialize(logfile=logfile)
+    Logging.get().info("TEST LOG")
     assert logfile.is_file()
     assert re.match(".+INFO.+TEST LOG$", logfile.read_text())
 
 def test_syslog_logging():
-    logger = Logging(syslog=True)
-    logger.get().info("TEST LOG SYSLOG")
+    Logging.initialize(syslog=True)
+    Logging.get().info("TEST LOG SYSLOG")
     found_log = False
     with open("/var/log/syslog", "r", encoding="utf-8") as syslog:
         for line in syslog:
@@ -52,14 +58,14 @@ def test_syslog_logging():
 
 def test_multi_dest_logging(capsys, path_generator):
     logfile = path_generator("yaesm_test_logging")
-    logger = Logging(stderr=True, logfile=logfile)
-    logger.get().info("TEST LOG MULTI DEST")
+    Logging.initialize(stderr=True, logfile=logfile)
+    Logging.get().info("TEST LOG MULTI DEST")
     assert re.match(".+INFO.+TEST LOG MULTI DEST$", capsys.readouterr().err)
     assert re.match(".+INFO.+TEST LOG MULTI DEST$", logfile.read_text())
 
 def test_disable_logging(capsys):
-    init_logging(stderr=True)
-    disable_logging()
+    Logging.initialize(stderr=True)
+    Logging.disable()
     with pytest.raises(LoggingNotInitializedException):
-        logger().info("TEST LOG DISABLED")
+        Logging.get().info("TEST LOG DISABLED")
     assert "" == capsys.readouterr().err
