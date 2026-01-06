68ae3b6e733f01b4c52dd7858066dc323954f570
SkiHatDuckie <SkiHatDuckie@gmail.com>
2026-01-06 00:12:58 -0500

    Cleanup cleanup + tests

--- src/yaesm/cleanup.py
+++ src/yaesm/cleanup.py
@@ -1,43 +1,48 @@
+"""src/yaesm/cleanup.py"""
+
 import atexit
 import signal
 import sys
 
-from yaesm.logging import logger
+from yaesm.logging import Logging
 
-_cleanup_functions = []
-_cleanup_initialized = False
+class Cleanup:
+    _functions = []
+    _initialized = False
 
-def initialize_cleanup():
-    """Initialize cleanup system for graceful shutdown.
+    @staticmethod
+    def initialize():
+        """Initialize cleanup system for graceful shutdown.
 
-    Sets up atexit handler and signal handlers (SIGTERM, SIGINT) to ensure
-    all registered cleanup functions are called when the program exits.
+        Sets up atexit handler and signal handlers (SIGTERM, SIGINT) to ensure
+        all registered cleanup functions are called when the program exits.
 
-    Should be called once during program initialization.
-    """
-    global _cleanup_initialized
-    if not _cleanup_initialized:
-        atexit.register(_do_cleanup)
-        signal.signal(signal.SIGTERM, _do_cleanup)
-        signal.signal(signal.SIGINT, _do_cleanup)
-        _cleanup_initialized = True
+        Should be called once during program initialization.
+        """
+        if not Cleanup._initialized:
+            atexit.register(Cleanup._do_cleanup)
+            signal.signal(signal.SIGTERM, Cleanup._do_cleanup)
+            signal.signal(signal.SIGINT, Cleanup._do_cleanup)
+            Cleanup._initialized = True
 
-def add_cleanup_function(func):
-    """Register a cleanup function to be run at program termination. Note that
-    the most recently registered functions get executed first.
-    """
-    _cleanup_functions.append(func)
+    @staticmethod
+    def add_function(func):
+        """Register a cleanup function to be run at program termination. Note that
+        the most recently registered functions get executed first.
+        """
+        Cleanup._functions.append(func)
 
-def _do_cleanup(*_args):
-    """Execute all registered cleanup functions such that the most recently
-    registered functions get executed first.
-    """
-    exit_status = 0
-    for func in reversed(_cleanup_functions):
-        try:
-            func()
-        except Exception as e:
-            exit_status = 1
-            logger().error(f"cleanup function failed: {e}", exc_info=True)
-    if _args: # called from signal handler, otherwise called from atexit
-        sys.exit(exit_status)
+    @staticmethod
+    def _do_cleanup(*_args):
+        """Execute all registered cleanup functions such that the most recently
+        registered functions get executed first.
+        """
+        exit_status = 0
+        for func in reversed(Cleanup._functions):
+            try:
+                func()
+            except Exception as e:
+                exit_status = 1
+                Logging.get().error("cleanup function failed: %s", e, exc_info=True)
+        if _args: # called from signal handler, otherwise called from atexit
+            sys.exit(exit_status)
