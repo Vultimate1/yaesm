87d4b0a22ee4132404e45b1367f80b9ff5fb96fb
SkiHatDuckie <SkiHatDuckie@gmail.com>
2026-01-11 23:45:38 -0500

    Cleanup sshtarget

--- src/yaesm/sshtarget.py
+++ src/yaesm/sshtarget.py
@@ -1,3 +1,5 @@
+"""src/yaesm/sshtarget.py"""
+
 import re
 import shlex
 import subprocess
@@ -20,7 +22,8 @@ class SSHTarget:
 
     Example::
         sshtarget = SSHTarget("ssh://p22:fred@fredserver:/backups", Path("/home/larry/.ssh/id_rsa"))
-        sshtarget = SSHTarget("ssh://fredhost:/backups, Path("/home/larry/.ssh/id_rsa"), sshconfig=Path("/home/larry/.ssh/larrys_ssh_config"))
+        sshtarget = SSHTarget("ssh://fredhost:/backups, Path("/home/larry/.ssh/id_rsa"), \
+            sshconfig=Path("/home/larry/.ssh/larrys_ssh_config"))
     """
     def __init__(self, target_spec, key:Path, sshconfig=None):
         self.key = Path(key)
@@ -29,7 +32,8 @@ class SSHTarget:
         if target_spec_re_result := self.is_sshtarget_spec(target_spec):
             self.spec = target_spec
             port = target_spec_re_result.group(1)
-            self.port = None if port is None else int(port[1:-1]) # strip off leading 'p' and trailing ':' from 'port'
+            # Strip off leading 'p' and trailing ':' from 'port'
+            self.port = None if port is None else int(port[1:-1])
             self.host = target_spec_re_result.group(2)
             self.path = Path(target_spec_re_result.group(3))
             if user_host_re_result := user_host_re.match(self.host):
@@ -64,7 +68,10 @@ class SSHTarget:
         """
         configfile_opt = "" if self.sshconfig is None else f"-F '{self.sshconfig}'"
         port_opt = "" if self.port is None else f"-p {self.port}"
-        return f"{extra_opts} -q -i '{self.key}' -o IdentitiesOnly=yes -o StrictHostKeyChecking=yes -o PasswordAuthentication=no -o ControlMaster=auto -o 'ControlPath=~/.ssh/yaesm-controlmaster-%r@%h:%p' -o ControlPersist=310 {configfile_opt} {port_opt}"
+        return f"{extra_opts} -q -i '{self.key}' -o IdentitiesOnly=yes \
+            -o StrictHostKeyChecking=yes -o PasswordAuthentication=no -o ControlMaster=auto \
+            -o 'ControlPath=~/.ssh/yaesm-controlmaster-%r@%h:%p' -o ControlPersist=310 \
+            {configfile_opt} {port_opt}"
 
     def openssh_cmd(self, cmd, extra_opts="", quote_cmd=True):
         """Returns a string of an OpenSSH command that executes 'cmd' on the
@@ -77,7 +84,9 @@ class SSHTarget:
         containing OpenSSH options.
 
         Example usage::
-            p = subprocess.run(sshtarget.openssh_cmd("btrfs send /home/fred/snapshots/snapshot12") + " | " + "btrfs receive /fred-home-backups/", shell=True, check=True, capture_output=True, encoding="utf-8")
+            p = subprocess.run(sshtarget.openssh_cmd("btrfs send /home/fred/snapshots/snapshot12") \
+                + " | " + "btrfs receive /fred-home-backups/", shell=True, check=True, \
+                capture_output=True, encoding="utf-8")
         """
         if quote_cmd:
             cmd = shlex.quote(cmd)
@@ -88,7 +97,7 @@ class SSHTarget:
         """Return True if we can establish a connection to the SSH target server
         and return False otherwise.
         """
-        return 0 == subprocess.run(self.openssh_cmd("exit 0"), shell=True).returncode
+        return 0 == subprocess.run(self.openssh_cmd("exit 0"), shell=True, check=False).returncode
 
     def is_dir(self, d=None):
         """Return True if 'd' is an existing directory on the remote SSH server.
@@ -96,7 +105,8 @@ class SSHTarget:
         """
         if d is None:
             d = self.path
-        return 0 == subprocess.run(self.openssh_cmd(f"[ -d '{d}' ]; exit $?"), shell=True).returncode
+        return 0 == subprocess.run(self.openssh_cmd(f"[ -d '{d}' ]; exit $?"), shell=True,
+                                   check=False).returncode
 
     def is_file(self, f=None):
         """Return True if 'f' is an existing file on the remote SSH server. If
@@ -104,7 +114,8 @@ class SSHTarget:
         """
         if f is None:
             f = self.path
-        return 0 == subprocess.run(self.openssh_cmd(f"[ -f '{f}' ]; exit $?"), shell=True).returncode
+        return 0 == subprocess.run(self.openssh_cmd(f"[ -f '{f}' ]; exit $?"), shell=True,
+                                   check=False).returncode
 
     def mkdir(self, d=None, parents=False, check=True):
         """Mkdir the directory 'd' on the remote SSH server. If 'd' is None,
@@ -115,7 +126,9 @@ class SSHTarget:
         if d is None:
             d = self.path
         p_flag = "-p" if parents else ""
-        return 0 == subprocess.run(self.openssh_cmd(f"if ! [ -d '{d}' ]; then mkdir {p_flag} '{d}'; fi"), shell=True, check=check).returncode
+        return 0 == subprocess.run(
+            self.openssh_cmd(f"if ! [ -d '{d}' ]; then mkdir {p_flag} '{d}'; fi"),
+                             shell=True, check=check).returncode
 
     def touch(self, f=None, check=True):
         """Touch the file 'f' on the remote SSH server. If 'f' is None then default
@@ -124,4 +137,5 @@ class SSHTarget:
         """
         if f is None:
             f = self.path
-        return 0 == subprocess.run(self.openssh_cmd(f"touch '{f}'"), shell=True, check=check).returncode
+        return 0 == subprocess.run(self.openssh_cmd(f"touch '{f}'"), shell=True,
+                                   check=check).returncode
