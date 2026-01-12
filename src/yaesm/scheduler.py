ed07e2dfd28fa35b8408b934a85f1a466eea1ba2
SkiHatDuckie <SkiHatDuckie@gmail.com>
2026-01-11 22:20:04 -0500

    Cleanup scheduler

--- src/yaesm/scheduler.py
+++ src/yaesm/scheduler.py
@@ -12,21 +12,20 @@ from yaesm.timeframe import Timeframe, FiveMinuteTimeframe, HourlyTimeframe, Dai
 class Scheduler:
     def __init__(self):
         self._apscheduler = apscheduler.schedulers.blocking.BlockingScheduler()
-        Logging.get('apscheduler').propagate = False
-        Logging.get('apscheduler').setLevel('CRITICAL')
+        Logging.get("apscheduler").propagate = False
+        Logging.get("apscheduler").setLevel("CRITICAL")
         self._apscheduler.add_listener(
-            lambda event: Logging.get().info(f"{self._job_name(event.job_id)} | successful backup"),
-            apscheduler.events.EVENT_JOB_EXECUTED
-        )
+            lambda event: Logging.get()
+                .info("%s - successful backup", self._job_name(event.job_id)),
+            apscheduler.events.EVENT_JOB_EXECUTED)
         self._apscheduler.add_listener(
-            lambda event: Logging.get().error(
-                f"{self._job_name(event.job_id)} | {event.exception}"),
-            apscheduler.events.EVENT_JOB_ERROR
-        )
+            lambda event: Logging.get()
+                .error("%s - %s", self._job_name(event.job_id), event.exception),
+            apscheduler.events.EVENT_JOB_ERROR)
         self._apscheduler.add_listener(
-            lambda event: Logging.get().error(f"{self._job_name(event.job_id)} | missed backup"),
-            apscheduler.events.EVENT_JOB_MISSED
-        )
+            lambda event: Logging.get()
+                .error("%s - missed backup", self._job_name(event.job_id)),
+            apscheduler.events.EVENT_JOB_MISSED)
 
     def start(self):
         """Start the scheduler. Since the scheduler blocks this function will
@@ -36,7 +35,7 @@ class Scheduler:
 
     def stop(self, force=False):
         """Stop the scheduler gracefully."""
-        self._apscheduler.shutdown(wait=not(force))
+        self._apscheduler.shutdown(wait=not force)
 
     def add_backups(self, backups):
         """Schedule every Backup in `backups` to have their backend's `do_backup()`
@@ -45,7 +44,8 @@ class Scheduler:
         for backup in backups:
             for timeframe in backup.timeframes:
                 job_name = f"{backup.name} ({timeframe.name})"
-                self._add_job(job_name, lambda b=backup, t=timeframe: b.backend.do_backup(b,t), timeframe)
+                self._add_job(job_name,
+                              lambda b=backup, t=timeframe: b.backend.do_backup(b,t), timeframe)
 
     def _job_name(self, job_id):
         """Return name of APScheduler job with job id `job_id`."""
@@ -66,17 +66,21 @@ class Scheduler:
             weekday_str = ",".join(str(Timeframe.weekday_num(d)) for d in timeframe.weekdays)
             for time in timeframe.times:
                 hour, minute = time
-                self._apscheduler.add_job(func, "cron", minute=minute, hour=hour, day_of_week=weekday_str, name=name)
+                self._apscheduler.add_job(func, "cron", minute=minute, hour=hour,
+                                          day_of_week=weekday_str, name=name)
         elif isinstance(timeframe, MonthlyTimeframe):
             for monthday in timeframe.monthdays:
                 for time in timeframe.times:
                     hour, minute = time
-                    self._apscheduler.add_job(func, "cron", minute=minute, hour=hour, day=monthday, name=name)
+                    self._apscheduler.add_job(func, "cron", minute=minute, hour=hour,
+                                              day=monthday, name=name)
         else: # YearlyTimeframe
             for yearday in timeframe.yeardays:
-                dt = datetime(1999, 1, 1) + timedelta(days=yearday-1) # Use non-leap year for conversion
+                # Use non-leap year for conversion
+                dt = datetime(1999, 1, 1) + timedelta(days=yearday-1)
                 month = dt.month
                 day = dt.day
                 for time in timeframe.times:
                     hour, minute = time
-                    self._apscheduler.add_job(func, "cron", minute=minute, hour=hour, day=day, month=month, name=name)
+                    self._apscheduler.add_job(func, "cron", minute=minute, hour=hour,
+                                              day=day, month=month, name=name)
